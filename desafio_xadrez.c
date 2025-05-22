77bad85c323baef758792e2286ee8baac0b55689
brads2501 <landimbruna71@gmail.com>
2025-05-22 10:52:52 -0300

    Update desafio_xadrez.c

--- desafio_xadrez.c
+++ desafio_xadrez.c
@@ -1,20 +1,23 @@
+// Criando código para movimentações de peças de xadrez.
+// Incluindo bibliotecas.
 #include <stdio.h>
-
+// Bloco de códigos que declaram os movimentos.
 int main() {
+  // Declarando o movimento da torre, para direita.
 printf("MOVIMENTO DA TORRE: \n");
 for (int i = 1; i <= 5; i++) {
   printf("DIREITA\n");
 
 }
 printf("\n");
-
+// Declarando o movimento do bispo, uma linha para cima e uma coluna para direita.
 printf("MOVIMENTO DO BISPO: \n");
 for (int i = 1; i <= 5; i++) {
   printf("CIMA, DIREITA\n");
 
 }
 printf("\n");
-
+// Declarando o movimento da rainha, uma coluna para esquerda.
 printf("MOVIMENTO DA RAINHA:\n");
 int movimentos_rainha = 0;
 while (movimentos_rainha < 8) {
@@ -22,7 +25,19 @@ while (movimentos_rainha < 8) {
   movimentos_rainha++;
 }
 printf("\n");
+// Declarando o movimento do cavalo, duas casas para baixo.
+printf("MOVIMENTO DO CAVALO:\n");
+for (int i = 0; i < 2; i++) {
+  printf("BAIXO\n");
+}
+// Movimentando uma casa para esquerda.
+int j = 0;
+while (j < 1) {
+  printf("ESQUERDA\n");
+  j++;
+}
+printf("\n");
 
-return 0;
+return 0; //Finalizando o programa.
 
 }
